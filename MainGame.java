08b8b477c48ebb5ec0f32668280e8593bbba69ef
aiji4879 <95641887+aiji4879@users.noreply.github.com>
2022-01-30 21:08:30 -0500

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -1,134 +1,200 @@
 /*
-* Authors: Edward Dong and Angelina Chen
-* Class Name: MainGame.java
-* Description of class: Sets up the game board, score, and keys that are needed to play the game
-*/
+Authors: Edward Dong and Angelina Chen
+Class name: MainGame.java
+Description: This is our main program. The board game is located in this class, and the user plays the game here.
+Score, board, and GUI logistics are all located in this file.
+ */
+
+//import necessary packages
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.*;
 
-/* calls and adds other classes to the main game
-*/
-public class MainGame extends JPanel implements KeyListener
-{
-    // declare variables
-    MainGrid grid = new MainGrid();
-
+public class MainGame extends JPanel implements KeyListener{
+    //declare default grid dimensions as 4, the variable will change if user has chosen an option
+    int gridSize = 4;
+    //make a new grid with the given dimensions
+    MainGrid game = new MainGrid(gridSize);
+    //allows the board to be displayed
+    String gameGrid = game.toString();
     static MainGame newGame = new MainGame();
+    //declare title of window
+    JFrame frame = new JFrame("2048");
+    int score = 0;
 
-    static JFrame frame = new JFrame("2048");
-
-    String gameGrid = grid.toString();
-    
-/* prints out score, basic instructions and clears the board when the game is over
-*/
-
-    public void paint(Graphics a)
+    /**
+     * this method returns the board
+     * @return the board
+     */
+    public static MainGame instance()
     {
-        super.paint(a);
-        Graphics2D a2 = (Graphics2D)a;
-        a2.drawString( "2048", 400, 60 );
-        a2.drawString( "Score: " + grid.getScore(), 200 - 4 * String.valueOf( grid.getScore() ).length(), 40 );
-        a2.drawString( "Highest Tile: " + grid.getHighestValue(), 280 - 4 * String.valueOf( grid.getHighestValue() ).length(), 40 );
-        a2.drawString( "Press Enter to Start", 210, 315 );
-        a2.drawString( "Use 'wasd' or Arrow Keys to move", 250, 500 );
+        return newGame;
+    }
 
+    //displays the main game when true
+    public void showGUI(boolean visible){
+        //adds key listener so user can use keys
+        frame.addKeyListener(newGame);
+        //sets basic operations for GUI to work
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(newGame);
+        frame.setSize(575, 575);
+        frame.setVisible(visible);
+        //makes new grid visible
+        game = new MainGrid(gridSize);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                score = game.getScore();
+                game.SaveScore();
+            }
+        });
+    }
 
-        if ( grid.gameOver() )
+    /**
+     * does nothing
+     * @param e the KeyEvent
+     */
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    /**
+     * determines what key the user has pressed and calls the method for that key, allowing them to move the tiles and play the game
+     * @param e the KeyEvent
+     */
+    @Override
+    public void keyPressed( KeyEvent e )
+    {
+        //'w' goes up
+        if ( e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP )
         {
-            a2.setColor( Color.gray );
-            a2.fillRect( 140, 50, 250, 250 );
-            for ( int i = 0; i < 4; i++ )
+            game.upKey();
+            game.getNumsOnGrid();
+            gameGrid = game.toString();
+            frame.repaint();
+        }
+        //'s' goes down
+        else if ( e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN )
+        {
+            game.downKey();
+            game.getNumsOnGrid();
+            gameGrid = game.toString();
+            frame.repaint();
+        }
+        //'a' goes left
+        else if ( e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT )
+        {
+            game.leftKey();
+            game.getNumsOnGrid();
+            gameGrid = game.toString();
+            frame.repaint();
+        }
+        //'d' goes right
+        else if ( e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT )
+        {
+            game.rightKey();
+            game.getNumsOnGrid();
+            gameGrid = game.toString();
+            frame.repaint();
+        }
+        //Enter key starts/restarts the game
+        else if ( e.getKeyCode() == KeyEvent.VK_ENTER )
+        {
+            game = new MainGrid(gridSize);
+            game.getNumsOnGrid();
+            game.getNumsOnGrid();
+            frame.repaint();
+        }
+        //Escape goes back to main menu
+        else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            score = MainGame.instance().game.points;
+            game.SaveScore();
+            frame.dispose();
+            new MainMenu();
+        }
+    }
+
+    /**
+     * does nothing
+     * @param e the KeyEvent
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    /**
+     * Updates the board every time tile is moved. It also tells indicates when the game is over for the user.
+     * Along with that, displays words to direct user on what to do, including displaying score.
+     * @param g the graphics parameter
+     */
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D)g;
+        //display info for user to read
+        g2.drawString( "Score: " + game.getScore(), 200 - gridSize * String.valueOf( game.getScore() ).length(), 40 );
+        g2.drawString( "Highest Tile: " + game.setHighestValue(), 280 - gridSize * String.valueOf( game.setHighestValue() ).length(), 40 );
+        g2.drawString( "Press Enter to Start", 150, 400 );
+        g2.drawString( "Use 'wasd' or Arrow Keys to move", 150, 350 );
+        //prompt the user to restart if board is full
+        if ( game.refreshBoard() ) {
+            g2.drawString("Press Enter to restart", 150, 450);
+            frame.repaint();
+        }
+        //repaints the board
+        g2.setColor( Color.gray );
+        g2.fillRect( 140, 50, gridSize*50+(gridSize+1)*10, gridSize*50+(gridSize+1)*10 );
+        for ( int i = 0; i < gridSize; i++ )
+        {
+            for ( int j = 0; j < gridSize; j++ )
             {
-                for ( int j = 0; j < 4; j++ )
+                drawTiles( g, game.grid[i][j], j * 60 + 150, i * 60 + 60 );
+            }
+        }
+        //all tiles display game over if user does not have anymore moves
+        if ( game.gameOver() )
+        {
+            g2.setColor( Color.gray );
+            g2.fillRect( 140, 50, gridSize*50+(gridSize+1)*10, gridSize*50+(gridSize+1)*10 );
+            for ( int i = 0; i < gridSize; i++ )
+            {
+                for ( int j = 0; j < gridSize; j++ )
                 {
-                    a2.setColor( Color.RED );
-                    a2.fillRoundRect( j * 60 + 150, i * 60 + 60, 50, 50, 5, 5 );
-                    a2.setColor( Color.black );
-                    a.drawString( "GAME", j * 60 + 160, i * 60 + 75 );
-                    a.drawString( "OVER", j * 60 + 160, i * 60 + 95 );
+                    g2.setColor( Color.RED );
+                    g2.fillRoundRect( j * 60 + 150, i * 60 + 60, 50, 50, 5, 5 );
+                    g2.setColor( Color.black );
+                    g.drawString( "GAME", j * 60 + 160, i * 60 + 75 );
+                    g.drawString( "OVER", j * 60 + 160, i * 60 + 95 );
                 }
             }
         }
     }
 
-/* sets the size of the GUI
-*/
-    public static void GUI()
-    {
-        // KeyListener receives keyboard events (keystrokes)
-        frame.addKeyListener(newGame);
-        frame.getContentPane().add(newGame);
-        frame.setSize( 850, 600 );
-        frame.setVisible( true );
-    }
-
-/* performs an action when a key is clicked
-*/
-    @Override
-    public void keyPressed( KeyEvent e )
-
-    {
-        // when it gets a keystorke, an action happens and changes the grid
-        if ( e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP )
-        {
-            grid.upKey();
-            grid.getNumsOnGrid();
-            gameGrid = grid.toString();
-            frame.repaint();
-        }
-        else if ( e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN )
-        {
-            grid.downKey();
-            grid.getNumsOnGrid();
-            gameGrid = grid.toString();
-            frame.repaint();
-        }
-        else if ( e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT )
-        {
-            grid.leftKey();
-            grid.getNumsOnGrid();
-            gameGrid = grid.toString();
-            frame.repaint();
-        }
-        else if ( e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT )
-        {
-            grid.rightKey();
-            grid.getNumsOnGrid();
-            gameGrid = grid.toString();
-            frame.repaint();
-        }
-        else if ( e.getKeyCode() == KeyEvent.VK_ENTER )
-        {
-            grid = new MainGrid();
-            grid.getNumsOnGrid();
-            grid.getNumsOnGrid();
-            frame.repaint();
-        }
-    }
-/* draws the tiles, board and colours it
-*/
-    public void drawTiles( Graphics a, Squares square, int x, int y )
-    {
+    /**
+     * This method draws each individual tile onto the grid
+     * @param g the graphics
+     * @param square each individual tile
+     * @param x the x-coordinate for where the tile is located
+     * @param y the y-coordinate for where the tile is located
+     */
+    public void drawTiles( Graphics g, Squares square, int x, int y ) {
         int tileValue = square.getValue();
-        int length = String.valueOf( tileValue ).length();
-        Graphics2D a2 = (Graphics2D)a;
-        a2.setColor( Color.lightGray );
-        a2.fillRoundRect( x, y, 50, 50, 5, 5 );
-        a2.setColor( Color.black );
-        if ( tileValue > 0 )
-        {
-            a2.setColor(square.getColor());
-            a2.fillRoundRect( x, y, 50, 50, 5, 5 );
-            a2.setColor( Color.black );
-            a.drawString( "" + tileValue, x + 25 - 3 * length, y + 25 );
+        int length = String.valueOf(tileValue).length();
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.lightGray);
+        g2.fillRoundRect(x, y, 50, 50, 5, 5);
+        g2.setColor(Color.black);
+        if (tileValue > 0) {
+            g2.setColor(square.getColour());
+            g2.fillRoundRect(x, y, 50, 50, 5, 5);
+            g2.setColor(Color.black);
+            g.drawString("" + tileValue, x + 25 - 3 * length, y + 25);
         }
-    }
-    
-// main method
-    public static void main( String[] args )
-    {
-        GUI();
     }
 
 }
